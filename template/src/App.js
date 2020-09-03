219c76e7563e0a8f996a1e807c956ee2c21b784c
David Isakson <ikeman32@yahoo.com>
2020-09-03 14:25:37 -0700

    Styling for app.js

--- template/src/App.js
+++ template/src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Routes from './components/routes/Routes';//Import routes
+import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
-    <div>
-      <header>
-        <nav>
-          <Link to="/">
-            Home
-          </Link>
-        </nav>
+    <div className="App">
+      <header className="App-header">
+        <p>React Electron Boilerplate</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Link to="/">Home</Link>
+        <Link to="/about">Link to the About Page</Link>
       </header>
       <>
-      <Routes />
+        <Routes />
       </>
     </div>
   );

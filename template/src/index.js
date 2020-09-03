0ed9ce61cb0f06b13ab2220459b7cffa48da6220
David Isakson <ikeman32@yahoo.com>
2020-09-03 14:26:53 -0700

    Configure for proper routing

--- template/src/index.js
+++ template/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
 import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { HashRouter as Router, Route } from 'react-router-dom'
+
+import Home from "./App"
+import About from "./pages/about"
 
 ReactDOM.render(
-    <Router><App /></Router>, 
-    document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <Router>
+        <div>
+            <main>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+            </main>
+        </div>
+    </Router>, 
+    document.getElementById("root")
+    
+)

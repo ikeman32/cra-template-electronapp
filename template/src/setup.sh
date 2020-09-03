f06a1dc0bc2e9d87d8718588e5e72a6ade96e2a3
David Isakson <ikeman32@yahoo.com>
2020-09-03 14:27:44 -0700

    Add echo statements and audit fix

--- template/src/setup.sh
+++ template/src/setup.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
 
-npm i && npm i concurrently electron electron-builder wait-on -D && rm setup.sh
+clear
+
+echo "Configuring project to run electronjs!"
+
+npm i && npm i concurrently electron electron-builder wait-on -D && npm audit fix && rm setup.sh
+
+clear
+
+echo "Project configured!"
+echo
+echo "You may now begin developing your desktop app using react."
+echo 
+echo "Develop normally as you would with a react app."
